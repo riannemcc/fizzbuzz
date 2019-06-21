849d2b8f894fbcab352ece17383769f8af740fe6
Rianne McCartney <riannemccartney@hotmail.com>
2019-06-21 14:49:36 +0100

    Fizzbuzz tests complete

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,6 @@
 def fizzbuzz(number)
-  'fizz'
+  return "fizzbuzz" if number % 3 == 0 && number % 5 == 0
+  return "fizz" if number % 3 == 0
+  return "buzz" if number % 5 == 0
+  return number 
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,13 @@ describe 'fizzbuzz' do
   it "returns 'fizz' when passed 3" do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it "returns 'buzz' when passed 5" do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it "returns 'fizzbuzz' when passed 15" do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it "returns 62 when passed 62" do
+    expect(fizzbuzz(62)).to eq 62
+  end
 end
